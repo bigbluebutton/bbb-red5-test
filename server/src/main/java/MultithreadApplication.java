d76eea2783481b606ba1455239e8486d6f1c6668
Richard Alam <ritzalam@gmail.com>
2013-03-04 19:33:00 +0000

    - print out scope lifecycle

--- server/src/main/java/MultithreadApplication.java
+++ server/src/main/java/MultithreadApplication.java
@@ -1,7 +1,7 @@
 import java.util.List;
 import java.util.concurrent.atomic.*;
-
 import org.red5.server.adapter.*;
+import org.red5.server.api.IClient;
 import org.red5.server.api.IConnection;
 import org.red5.server.api.scope.IScope;
 import org.red5.server.api.service.ServiceUtils;
@@ -44,7 +44,75 @@ public class MultithreadApplication extends MultiThreadedApplicationAdapter {
 			so.sendMessage("receiveMessage", params);
 		}		
 		
-		ServiceUtils.in(conn, "receiveMessage", params.toArray());
-		ServiceUtils.
+//		ServiceUtils.(conn, "receiveMessage", params.toArray());
+	}
+	
+	@Override
+	public boolean appStart(IScope app) {
+		System.out.println("**************** App Start ****************************");
+		return super.appStart(app);
+	}
+
+	@Override
+	public boolean appConnect(IConnection conn, Object[] params) {
+		System.out.println("**************** App Connect ****************************");
+		return super.appConnect(conn, params);
+	}
+
+	@Override
+	public boolean appJoin(IClient client, IScope app) {
+		System.out.println("**************** App Join ****************************");
+		return super.appJoin(client, app);
+	}
+
+	@Override
+	public void appDisconnect(IConnection conn) {
+		System.out.println("**************** App Disconnect ****************************");
+	}
+
+	@Override
+	public void appLeave(IClient client, IScope app) {
+		System.out.println("**************** App Leave ****************************");
+	}
+
+	@Override
+	public void appStop(IScope app) {
+		System.out.println("**************** App Stop ****************************");
+	}
+
+	@Override
+	public boolean roomStart(IScope room) {
+		System.out.println("**************** Room Start ****************************");
+		return super.roomStart(room);
+	}
+
+	@Override
+	public boolean roomConnect(IConnection conn, Object[] params) {
+		System.out.println("**************** Room Connect ****************************");
+		return super.roomConnect(conn, params);
+	}
+
+	@Override
+	public boolean roomJoin(IClient client, IScope room) {
+		System.out.println("**************** Room Join ****************************");
+		return super.roomJoin(client, room);		
+	}
+
+	@Override
+	public void roomDisconnect(IConnection conn) {
+		System.out.println("**************** Room Disconnect ****************************");
+		super.roomDisconnect(conn);			
+	}
+
+	@Override
+	public void roomLeave(IClient client, IScope room) {
+		System.out.println("**************** Room Leave ****************************");
+		super.roomLeave(client, room);			
+	}
+
+	@Override
+	public void roomStop(IScope room) {
+		System.out.println("**************** Room Stop ****************************");
+		super.roomStop(room);				
 	}
 }
